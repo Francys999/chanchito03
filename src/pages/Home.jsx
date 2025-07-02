8767e9d5ae560a4c826bb71cd9f60ef0509c50c1
Francys999 <francis11444@gmail.com>
2025-07-02 16:59:12 -0500

    arreglando lista de perritos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="container">
-            <h1>Comprobando pagina de perritos</h1>
+            <h1>Ir a CardList para ir a la lista de perritos</h1>
         </div>
     );
 }
